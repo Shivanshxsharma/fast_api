ec464615adbb8ce1172883c016979b31b1dc5fef
Shivanshxsharma <shivanshsharma9858@gmail.com>
2025-12-27 00:13:46 +0530

    add initial post_routes.py with request body handling comments

---
+++ post_routes.py
@@ -0,0 +1,17 @@
+# a request body is  the posrtion of HTTP request that conatins data sent by the client to the srever . it is typically used in HTTP methods
+#such as POST Or PUT to transmit structured data JSON for the purpose of creating  or updating the  request body resources on the server.  and
+# the server passes the body to extract the necessary information and perform the intended operation
+
+
+# app---------->server
+#  |             ^
+#  |-------------|
+         #JSON
+
+
+
+
+# steps 
+# 1 data sent by client
+# 2 validation  of received from client side
+# 3 creation of json file in database
